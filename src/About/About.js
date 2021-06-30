fab7552066f087fdaf71c797b7be370fc0e2c4ea
Nayemul <nayemulhasan2016@gmail.com>
2021-06-30 22:01:48 +0600

    modify

--- src/About/About.js
+++ src/About/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
         <div className='text-center'>
             <h1 className='text-3xl mt-3 text-gray-400'><span className="text-5xl ">NBlog</span> is a online bogging platform</h1>
             <p>Coming soon....</p>
+            <Link to="/">go to home</Link>
         </div>
     )
 }
